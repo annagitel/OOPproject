306e72c5403bf62687cd362cdf1f675ab3eed558
annagitel <annagitel39@gmail.com>
2019-11-19 11:22:27 +0200

    first commit of the project

---
+++ src/myMath/Monom.java
@@ -0,0 +1,225 @@
+
+package myMath;
+
+import java.net.StandardSocketOptions;
+import java.util.Comparator;
+
+/**
+ * This class represents a simple "Monom" of shape a*x^b, where a is a real number and a is an integer (summed a none negative), 
+ * see: https://en.wikipedia.org/wiki/Monomial 
+ * The class implements function and support simple operations as: construction, value at x, derivative, add and multiply. 
+ * @author Boaz
+ * @author anna
+ */
+public class Monom implements function{
+	public static final Monom ZERO = new Monom(0,0);
+	public static final Monom MINUS1 = new Monom(-1,0);
+	public static final double EPSILON = 0.0000001;
+
+
+	public static final Comparator<Monom> _Comp = new Monom_Comperator();
+	public static Comparator<Monom> getComp() {return _Comp;}
+	public Monom(double a, int b){
+		this.set_coefficient(a);
+		this.set_power(b);
+	}
+	public Monom(Monom ot) {
+		this(ot.get_coefficient(), ot.get_power());
+	}
+	
+	public double get_coefficient() {
+		return this._coefficient;
+	}
+
+	public int get_power() {
+		return this._power;
+	}
+	/** 
+	 * this method returns the derivative monom of this.
+	 * @return
+	 */
+	public Monom derivative() {
+		if(this.get_power()==0) {return getNewZeroMonom();}
+		return new Monom(this.get_coefficient()*this.get_power(), this.get_power()-1);
+	}
+
+	public double f(double x) {
+		double ans=0;
+		double p = this.get_power();
+		ans = this.get_coefficient()*Math.pow(x, p);
+		return ans;
+	}
+
+	public boolean isZero() {
+		return this.get_coefficient() == 0;
+	}
+
+	// ***************** add your code below **********************
+	public Monom(String s) throws RuntimeException{
+		if (!isMonom(s))
+			throw new RuntimeException("The string is not a valid Monom format");
+		else {
+			int pow = 0;
+			int x = 0;
+			for (int i=0;i<s.length();i++){
+				if (s.charAt(i) == 'x') {x++;}
+				if (s.charAt(i) == '^') {pow++;}
+			}
+			if (x==0)
+				set_coefficient(Double.parseDouble(s));
+			else if (x==1) {
+				if (s.indexOf('x') == 0)
+					set_coefficient(1);
+				else if (s.indexOf('-') == 0 && s.indexOf('x') == 1)
+					set_coefficient(-1);
+				else
+					set_coefficient(Double.parseDouble(s.substring(0, s.indexOf('x'))));
+			}
+			if (pow==0) {
+				if (x==1)
+					set_power(1);
+				else
+					set_power(0);
+			}
+			else if (pow==1)
+				set_power(Integer.parseInt(s.substring(s.indexOf('^')+1,s.length())));
+		}
+	}
+
+	public Monom negative(){
+		Monom negM = new Monom((0-this.get_coefficient()), this.get_power());
+		return negM;
+	}
+	
+	public void add(Monom m) throws RuntimeException {
+		if (get_power() == m.get_power())
+			set_coefficient(get_coefficient()+m.get_coefficient());
+		else
+			throw new RuntimeException("ERR the power of the monoms should be the same. you have got:"+m.get_power()+" "+get_power());
+	}
+	
+	public void multipy(Monom d) {
+		if (d.isZero()){
+			set_coefficient(0);
+			set_power(0);
+		}
+		else if (d.get_power()== 0)
+			set_coefficient(get_coefficient()*d.get_coefficient());
+		else{
+			set_power(get_power()+d.get_power());
+			set_coefficient(get_coefficient()*d.get_coefficient());
+		}
+	}
+	
+	public String toString() {
+		Double coef = get_coefficient();
+		int pow = get_power();
+		String coefs = "";
+		String pows = "";
+
+		if(coef == 0)
+			return "0";
+		if (pow==0)
+			return Double.toString(coef);
+
+		if (pow == 1)
+			pows = pows + "x";
+		else
+			pows = "x^" +Integer.toString(pow);
+
+		if (coef == -1)
+			coefs = "-";
+		else if (coef == 1)
+			coefs = "";
+		else
+			coefs = Double.toString(coef);
+
+		return (coefs+pows);
+	}
+
+	public boolean equals(Monom a) {
+		if (a.get_coefficient()==0 && this.get_coefficient()==0)
+			return true;
+		if (a.get_power()==this.get_power() && a.get_coefficient()==this.get_coefficient())
+			return true;
+		return false;
+	}
+
+	//****************** Private Methods and Data *****************
+	private Boolean isMonom(String s){
+		int dot = 0;
+		int pow = 0;
+		int x = 0;
+		int minus = 0;
+		for (int i=0;i<s.length();i++){
+			if (s.charAt(i) == '.') {dot++;}
+			if (s.charAt(i) == 'x') {x++;}
+			if (s.charAt(i) == '^') {pow++;}
+			if (s.charAt(i) == '-') {minus++;}
+		}
+		if ((minus>1)||(dot>1)||(pow>1)||(x>1)) //not more than one x^-.
+			return false;
+		if (minus==1 && (s.indexOf('-')!=0 || s.indexOf('-')==s.length()-1)) // - must be first and not last
+			return false;
+		if ((pow==1) && (x!=1)) // cant be ^ without x
+			return false;
+		if(s.indexOf('^')==s.length()-1) // ^ cant be last
+		 	return false;
+		if (x==0 && pow==0){ //number only case
+			return check_coef(s);
+		}
+		if (x==1 && pow==0){  //no power case
+			if (!check_coef(s.substring(0,s.indexOf('x'))) || s.indexOf('x')!=s.length()-1)
+				return false;
+		}
+		if (x==1 && pow==1){   //full monom case
+			if ((s.indexOf('^') - s.indexOf('x')) != 1)
+				return false;
+			if (!check_coef(s.substring(0,s.indexOf('x'))) || !check_pow(s.substring(s.indexOf('^')+1,s.length())))
+				return false;
+		}
+		return true;
+	}
+	private boolean check_coef(String s){
+		if (s.length()==0)
+			return true;
+		if (s.length()<2){
+			if (s.charAt(0)=='-' || ((s.charAt(0)>='0' && s.charAt(0)<='9')) ){
+				return true;
+			}
+		}
+		for (int i=0;i<s.length();i++){
+			char c = s.charAt(i);
+			if (c=='-' || c=='.' ||(c>='0' && c<='9')){
+				if(c=='.'){
+					if (s.charAt(i-1)<'0' || s.charAt(i-1)>'9'|| s.charAt(i+1)<'0' || s.charAt(i+1)>'9')
+						return false;
+				}
+			}
+			if ((s.charAt(0)=='-' && s.charAt(1)=='0' && s.charAt(2)!='.') || (s.charAt(0)=='0' && s.charAt(1)!='.'))
+				return false;
+		}
+		return true;
+	}
+	private boolean check_pow(String s){
+		if(s.charAt(0)=='0')
+			return false;
+		for(int i=0;i<s.length();i++){
+			if (s.charAt(i)<'0'|| s.charAt(i)>'9')
+				return false;
+		}
+		return true;
+	}
+	private void set_coefficient(double a){
+		this._coefficient = a;
+	}
+	private void set_power(int p) {
+		if(p<0) {throw new RuntimeException("ERR the power of Monom should not be negative, got: "+p);}
+		this._power = p;
+	}
+	private static Monom getNewZeroMonom() {return new Monom(ZERO);}
+	private double _coefficient; 
+	private int _power;
+	
+	
+}
---
+++ src/myMath/Polynom.java
@@ -0,0 +1,273 @@
+package myMath;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+import myMath.Monom;
+/**
+ * This class represents a Polynom with add, multiply functionality, it also should support the following:
+ * 1. Riemann's Integral: https://en.wikipedia.org/wiki/Riemann_integral
+ * 2. Finding a numerical value between two values (currently support root only f(x)=0).
+ * 3. Derivative
+ * 
+ * @author Boaz
+ * @author anna
+ */
+public class Polynom implements Polynom_able{
+	/************************************constractors******************************************************************/
+	ArrayList<Monom> currentPolynom; //INIT THE POLYNOM AS AN ARRAY LIST. THE CONSTRACTORS USE THIS AL
+	Monom_Comperator sortComp = new Monom_Comperator();
+
+	/*** Zero (empty polynom)*/
+	public Polynom() {
+		currentPolynom = new ArrayList<Monom>(0); // Create an ArrayList object;
+	}
+	/*** init a Polynom from a String* @param s: is a string represents a Polynom*/
+	public Polynom(String s) {
+		currentPolynom = new ArrayList<Monom>(); // Create an ArrayList object;
+		String currentS = "";
+		Monom currentMonom;
+		for (int i=0;i<s.length();i++){
+			if (i==0 && s.charAt(i)=='-')
+				currentS += '-';
+			if(s.charAt(i)!='+' && s.charAt(i)!='-')
+				currentS = currentS+s.charAt(i);
+			else if (!currentS.equals("-")){
+				currentMonom = new Monom(currentS);
+				currentPolynom.add(currentMonom);
+				if (s.charAt(i)=='-')
+					currentS = "-";
+				else
+					currentS = "";
+			}
+		}
+		currentMonom = new Monom(currentS);
+		currentPolynom.add(currentMonom);
+
+		currentPolynom.sort(sortComp);
+	}
+
+	/*** init a Polynom from an Excisting polynom (deep copy):*/
+	public Polynom(Polynom p) {
+		currentPolynom = new ArrayList<Monom>();
+		Iterator<Monom> monoms = p.iteretor();
+		while(monoms.hasNext()) {
+			Monom a = new Monom(monoms.next());
+			add(a);
+		}
+	}
+
+	/************************************public methods ******************************************************************/
+	@Override
+	public double f(double x) {
+		// TODO Auto-generated method stub
+		double sum = 0;
+		Iterator<Monom> polynomIterator = this.iteretor();
+		while(polynomIterator.hasNext()){
+			Monom currentMonom = polynomIterator.next() ;
+			sum = sum + currentMonom.f(x);
+		}
+		return sum;
+	}
+
+	@Override
+	public void add(Monom m1) {
+		// TODO Auto-generated method stub
+		Iterator<Monom> polynomIterator = currentPolynom.iterator();
+		while (polynomIterator.hasNext())
+		{
+			Monom currentMonom=polynomIterator.next();
+			if (currentMonom.get_power()==m1.get_power()) {
+				currentMonom.add(m1);
+				if (currentMonom.isZero()) {
+					currentPolynom.remove(currentMonom);
+				}
+				return;
+			}
+		}
+		currentPolynom.add(m1);
+		currentPolynom.sort(sortComp);
+	}
+
+	@Override
+	public void add(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		Iterator<Monom> monomIterator = p1.iteretor();
+		while (monomIterator.hasNext())
+			this.add(monomIterator.next());
+	}
+
+	@Override
+	public void substract(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		Iterator<Monom> monomIterator = p1.iteretor();
+		Polynom negPoly = new Polynom();
+		while (monomIterator.hasNext()) {
+			negPoly.add(monomIterator.next().negative());
+		}
+		this.add(negPoly);
+	}
+
+	@Override
+	public void multiply(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		Iterator<Monom> thisMonomIterator = this.iteretor();
+		Iterator<Monom> givenMonomIterator = p1.iteretor();
+		Polynom newPolynom =new Polynom();
+
+		while (thisMonomIterator.hasNext()){
+			Monom thisMonom = thisMonomIterator.next();
+			givenMonomIterator = p1.iteretor();
+			while (givenMonomIterator.hasNext()) {
+				Monom givenMonom =givenMonomIterator.next();
+				Monom newMonom = new Monom(thisMonom);
+				newMonom.multipy(givenMonom);
+				newPolynom.add(newMonom);
+			}
+		}
+
+		currentPolynom.clear();
+		this.add(newPolynom);
+	}
+
+	@Override
+	public boolean equals(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		Iterator<Monom> thisMonomIterator = this.iteretor();
+		Iterator<Monom> givenMonomIterator = p1.iteretor();
+
+		while(givenMonomIterator.hasNext() && thisMonomIterator.hasNext()){
+			if(!givenMonomIterator.next().equals(thisMonomIterator.next()))
+				return false;
+		}
+		if(givenMonomIterator.hasNext() || thisMonomIterator.hasNext())
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public boolean isZero() {
+		// TODO Auto-generated method stub
+		ArrayList<Monom> newPoly = new ArrayList<Monom>();
+		Iterator<Monom> monomIterator = this.iteretor();
+		while (monomIterator.hasNext()){
+			Monom current = monomIterator.next();
+			if (!current.isZero())
+				return false;
+		}
+		return true;
+	}
+
+	@Override
+	public double root(double x0, double x1, double eps) {
+		// TODO Auto-generated method stub
+		if (x0 *x1 <= 0 || f(x0)*f(x1)<= 0) {
+			double middle = (x1 + x0) / 2;
+			if (Math.abs(f(middle)) < eps)
+				return middle;
+			if (f(x0) == 0)
+				return x0;
+			if (f(x1) == 0)
+				return x1;
+			if (f(middle) < 0)
+				x0 = middle;
+			else if (f(middle) > 0)
+				x1 = middle;
+			return root(x0, x1, eps);
+		}
+		else {
+			throw new RuntimeException("cannot be calculated, Not root value");
+		}
+
+	}
+
+	@Override
+	public Polynom_able copy(){
+		// TODO Auto-generated method stub
+		ArrayList<Monom> newPoly = new ArrayList<Monom>();
+		Iterator<Monom> monomIterator = this.iteretor();
+		while (monomIterator.hasNext()){
+			Monom current = monomIterator.next();
+			Monom newMonom = new Monom(current.get_coefficient(),current.get_power());
+			newPoly.add(newMonom);
+		}
+		return null;
+	}
+
+	@Override
+	public Polynom_able derivative() {
+		// TODO Auto-generated method stub
+			Polynom derivatedPolynom = new Polynom();
+			Iterator<Monom> monomIterator = this.iteretor();
+			while (monomIterator.hasNext()) {
+				Monom current = monomIterator.next();
+				Monom derivated = new Monom(current).derivative();
+				if (derivated.get_coefficient()!=0)
+					derivatedPolynom.add(derivated);
+			}
+		return derivatedPolynom;
+	}
+
+	@Override
+	public double area(double x0, double x1, double eps) {
+		// TODO Auto-generated method stub
+		double sum =0;
+		double min = Math.min(x0,x1);
+		double max = Math.max(x0,x1);
+
+		for(double i=min+eps;i<=x1;i+=eps){
+			if (f(i)>0){
+				sum +=f(i)*eps;
+			}
+		}
+		return sum;
+	}
+
+	public Iterator<Monom> iteretor() {
+		// TODO Auto-generated method stub
+		return currentPolynom.iterator();
+	}
+	@Override
+	public void multiply(Monom m1) {
+		// TODO Auto-generated method stub
+		Iterator<Monom> monomIterator = this.iteretor();
+		while (monomIterator.hasNext()){
+			Monom current = monomIterator.next();
+			current.multipy(m1);
+		}
+	}
+
+	public String toString() {
+		String s ="";
+		Monom m;
+		Iterator <Monom> monomIterator=currentPolynom.iterator();
+		while(monomIterator.hasNext()) {
+			m=monomIterator.next();
+			if(s.length()==0)
+				s = s+m.toString();
+			else{
+				if(m.get_coefficient()<0)
+					s=s+m.toString();
+				else
+					s=s+'+'+m.toString();
+			}
+		}
+		return s;
+	}
+
+	public int size(){
+		int count = 0;
+		Iterator <Monom> monomIterator=this.iteretor();
+		while(monomIterator.hasNext()){
+			count++;
+			monomIterator.next();
+		}
+		return count;
+	}
+
+	/************************************private methods ******************************************************************/
+
+	
+}
